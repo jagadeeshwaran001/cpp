e304d9d18b8920babf6a6d6c1bc92e3da9af18cc
jagadeeshwaran001 <45691973+jagadeeshwaran001@users.noreply.github.com>
2020-03-05 11:40:05 +0530

    Add files via upload

---
+++ Number_Mirror_CodeChef.cpp
@@ -0,0 +1,10 @@
+//link for the problem:-https://www.codechef.com/problems/START01
+#include <iostream>
+using namespace std;
+
+int main() {
+    long int a;
+    cin>>a;
+    cout<<"\n"<<a;
+	return 0;
+}
